e0ae7744293e311cbf6183c91767981a59f833e1
karshfish <karshfish35@gmail.com>
2025-10-06 11:34:19 +0300

    idk

--- Angular/lab2/src/app/products/products.ts
+++ Angular/lab2/src/app/products/products.ts
@@ -1,34 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product.model';
-import { CommonModule } from '@angular/common';   
+import { CommonModule } from '@angular/common';
 import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.html',
-    standalone: true,             
-  imports: [CommonModule],      
-  styleUrls: ['./products.css']
+  standalone: true,
+  imports: [CommonModule],
+  styleUrls: ['./products.css'],
 })
 export class Products implements OnInit {
   products: Product[] = [];
   loading = true;
-  
+
   error: string | null = null;
 
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe({
-     next: (data) => {
-  this.products = data.products;   
-  this.loading = false;
-}
-,
+      next: (data) => {
+        this.products = data.products;
+        this.loading = false;
+      },
       error: () => {
         this.error = 'Failed to load products';
         this.loading = false;
-      }
+      },
     });
   }
 }
